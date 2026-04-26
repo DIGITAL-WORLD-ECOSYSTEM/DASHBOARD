68ded69a7568d81c3cb71e300eef06ed901ece72
associaasppibra <associaasppibra@gmail.com>
2026-04-26 01:00:33 +0000

    Initial commit

---
+++ src/layouts/nav-config-dashboard.tsx
@@ -0,0 +1,133 @@
+import type { NavSectionProps } from 'src/components/nav-section';
+
+import { paths } from 'src/routes/paths';
+
+import { CONFIG } from 'src/global-config';
+
+import { Label } from 'src/components/label';
+import { SvgColor } from 'src/components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name: string) => (
+  <SvgColor src={`${CONFIG.assetsDir}/assets/icons/navbar/${name}.svg`} />
+);
+
+const ICONS = {
+  blog: icon('ic-blog'),
+  chat: icon('ic-chat'),
+  mail: icon('ic-mail'),
+  user: icon('ic-user'),
+  file: icon('ic-file'),
+  lock: icon('ic-lock'),
+  order: icon('ic-order'),
+  label: icon('ic-label'),
+  blank: icon('ic-blank'),
+  folder: icon('ic-folder'),
+  params: icon('ic-params'),
+  banking: icon('ic-banking'),
+  invoice: icon('ic-invoice'),
+  product: icon('ic-product'),
+  calendar: icon('ic-calendar'),
+  disabled: icon('ic-disabled'),
+  external: icon('ic-external'),
+  subpaths: icon('ic-subpaths'),
+  menuItem: icon('ic-menu-item'),
+  ecommerce: icon('ic-ecommerce'),
+  analytics: icon('ic-analytics'),
+  dashboard: icon('ic-dashboard'),
+};
+
+// ----------------------------------------------------------------------
+
+export const navData: NavSectionProps['data'] = [
+  /**
+   * Overview
+   */
+  {
+    subheader: 'Overview',
+    items: [
+      { title: 'App', path: paths.dashboard.root, icon: ICONS.dashboard },
+      { title: 'Ecommerce', path: paths.dashboard.general.ecommerce, icon: ICONS.ecommerce },
+      { title: 'Analytics', path: paths.dashboard.general.analytics, icon: ICONS.analytics },
+      { title: 'Banking', path: paths.dashboard.general.banking, icon: ICONS.banking },
+      { title: 'File', path: paths.dashboard.general.file, icon: ICONS.file },
+    ],
+  },
+  /**
+   * Management
+   */
+  {
+    subheader: 'Management',
+    items: [
+      {
+        title: 'User',
+        path: paths.dashboard.user.root,
+        icon: ICONS.user,
+        children: [
+          { title: 'Profile', path: paths.dashboard.user.root },
+          { title: 'Cards', path: paths.dashboard.user.cards },
+          { title: 'List', path: paths.dashboard.user.list },
+          { title: 'Create', path: paths.dashboard.user.new },
+          { title: 'Edit', path: paths.dashboard.user.demo.edit },
+          { title: 'Account', path: paths.dashboard.user.account, deepMatch: true },
+        ],
+      },
+      {
+        title: 'Product',
+        path: paths.dashboard.product.root,
+        icon: ICONS.product,
+        children: [
+          { title: 'List', path: paths.dashboard.product.root },
+          { title: 'Details', path: paths.dashboard.product.demo.details },
+          { title: 'Create', path: paths.dashboard.product.new },
+          { title: 'Edit', path: paths.dashboard.product.demo.edit },
+        ],
+      },
+      {
+        title: 'Order',
+        path: paths.dashboard.order.root,
+        icon: ICONS.order,
+        children: [
+          { title: 'List', path: paths.dashboard.order.root },
+          { title: 'Details', path: paths.dashboard.order.demo.details },
+        ],
+      },
+      {
+        title: 'Invoice',
+        path: paths.dashboard.invoice.root,
+        icon: ICONS.invoice,
+        children: [
+          { title: 'List', path: paths.dashboard.invoice.root },
+          { title: 'Details', path: paths.dashboard.invoice.demo.details },
+          { title: 'Create', path: paths.dashboard.invoice.new },
+          { title: 'Edit', path: paths.dashboard.invoice.demo.edit },
+        ],
+      },
+      {
+        title: 'Blog',
+        path: paths.dashboard.post.root,
+        icon: ICONS.blog,
+        children: [
+          { title: 'List', path: paths.dashboard.post.root },
+          { title: 'Details', path: paths.dashboard.post.demo.details },
+          { title: 'Create', path: paths.dashboard.post.new },
+          { title: 'Edit', path: paths.dashboard.post.demo.edit },
+        ],
+      },
+      { title: 'File manager', path: paths.dashboard.fileManager, icon: ICONS.folder },
+      {
+        title: 'Mail',
+        path: paths.dashboard.mail,
+        icon: ICONS.mail,
+        info: (
+          <Label color="error" variant="inverted">
+            +32
+          </Label>
+        ),
+      },
+      { title: 'Chat', path: paths.dashboard.chat, icon: ICONS.chat },
+      { title: 'Calendar', path: paths.dashboard.calendar, icon: ICONS.calendar },
+    ],
+  },
+];
