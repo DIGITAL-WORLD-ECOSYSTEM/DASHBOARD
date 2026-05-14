e6d91ac519244de10767e3ed822b8539af974a4d
ASPPIBRA-DEV <sandro_ceo@asppibra.com.br>
2026-05-14 15:15:28 -0300

    fix(build): simplify chunking to prevent loading errors

--- vite.config.ts
+++ vite.config.ts
@@ -43,4 +43,12 @@ export default defineConfig({
   },
   server: { port: PORT, host: true },
   preview: { port: PORT, host: true },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
